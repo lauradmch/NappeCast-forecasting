5d81fc83283fd8a4a8e5be750626406211c040a1
adriencosson-beep <adrien.cosson@gmail.com>
2026-07-29 00:25:23 +0200

    feat: final version of the preprossing.py with selection of the target and the feature_cols

--- src/models/preprocessing.py
+++ src/models/preprocessing.py
@@ -2,40 +2,39 @@
 import pandas as pd
 
 
-def preprocessing(df):
+def preprocessing(df, test_size=30):
     """
-    This function performed the following steps:
-    1. preprocessing preparation (dropping undesire columns; splitting the target 'y' and the explicatives features 'X')
-    2. Temporal split (X_train; X_test; y_train & y_test)
-    At the end, this function return the different datasets to perform Machine Learning algorithms.
+    Steps:
+    1. Set temporal index
+    2. Select relevant daily features (numeric, domain-informed)
+    3. Temporal train/test split (last test_size days = test set)
+    4. Split X / y
+    Returns X_train, X_test, y_train, y_test.
     """
-    # Setting 'date_index' as index in the dataframe
+    df = df.copy()
     df = df.set_index("date_index", drop=False)
     df.index = pd.to_datetime(df.index)
-    
-    # Build the monthly panel
-    std_cols = df.filter(regex=r"^S.*I$").columns
-    df = df.filter(std_cols)
-    df_monthly = df.groupby(df.index.to_period('M')).mean()
-    
-    # Definition of the target
-    df_monthly['SPLI_1m'] = df_monthly['SPLI'].shift(-1) # SPLI from the next month
-    df_monthly['SPLI_2m'] = df_monthly['SPLI'].shift(-2) # SPLI from the next two months
-    df_monthly['SPLI_3m'] = df_monthly['SPLI'].shift(-3) # SPLI from the next three months
-    
-    # Temporal split (80/20% for the train/test sets split)
-    df_train = df_monthly.iloc[:-7]
-    df_test = df_monthly.iloc[-7:-4]
-    
-    # Split X/y     
-    y_train_1m = df_train['SPLI_1m']
-    y_train_2m = df_train['SPLI_2m']
-    y_train_3m = df_train['SPLI_3m']
-    X_train = df_train.drop(columns=['SPLI', 'SPLI_1m', 'SPLI_2m', 'SPLI_3m'])
-   
-    y_test_1m = df_test['SPLI_1m']
-    y_test_2m = df_test['SPLI_2m']
-    y_test_3m = df_test['SPLI_3m']
-    X_test = df_test.drop(columns=['SPLI', 'SPLI_1m', 'SPLI_2m', 'SPLI_3m'])
-    
-    return X_train, y_train_1m, y_train_2m, y_train_3m, X_test, y_test_1m, y_test_2m, y_test_3m
+    df = df.sort_index()
+
+    feature_cols = [
+        "shortwave_radiation_sum",
+        "et0_fao_evapotranspiration",
+        "soil_temperature_0_to_100cm_mean",
+        "P_cum_90d",
+        "Peff_cum_90d",
+        "Temperature_mean_90d"
+    ]
+    target_col = "niveau_nappe_eau"
+
+    df = df[feature_cols + [target_col]].dropna()
+
+    test_size = test_size # days as the dataframe is daily
+    df_train = df.iloc[:-test_size]
+    df_test  = df.iloc[-test_size:]
+
+    X_train = df_train[feature_cols]
+    y_train = df_train[target_col]
+    X_test  = df_test[feature_cols]
+    y_test  = df_test[target_col]
+
+    return X_train, X_test, y_train, y_test
